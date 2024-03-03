6ea2c39255c3b20736760a654e11969c639241e7
Yani Jivkov <114335628+Yani-Jivkov@users.noreply.github.com>
2024-03-03 15:21:31 +0200

    ENCnDNC.py

--- ENCnDNC.py
+++ ENCnDNC.py
@@ -1,119 +1,26 @@
 def ENC_logic(): # Main ENC logic
-    crypt_word = input()
+    crypt_word = input('Enter the word => ')
     new_word = []
     # For loop
-    for i in range(len(crypt_word) + 1):
-        if i == 1:
-            char1 = ord(crypt_word[0])
-            char1 -= 1
-            char1 = chr(char1)
-            new_word.append(char1)
-        elif i == 2:
-            char2 = ord(crypt_word[1])
-            char2 -= 2
-            char2 = chr(char2)
-            new_word.append(char2)
-        elif i == 3:
-            char3 = ord(crypt_word[2])
-            char3 -= 3
-            char3 = chr(char3)
-            new_word.append(char3)
-        elif i == 4:
-            char4 = ord(crypt_word[3])
-            char4 -= 4
-            char4 = chr(char4)
-            new_word.append(char4)
-        elif i == 5:
-            char5 = ord(crypt_word[4])
-            char5 -= 5
-            char5 = chr(char5)
-            new_word.append(char5)
-        elif i == 6:
-            char6 = ord(crypt_word[5])
-            char6 -= 6
-            char6 = chr(char6)
-            new_word.append(char6)
-        elif i == 7:
-            char7 = ord(crypt_word[6])
-            char7 -= 7
-            char7 = chr(char7)
-            new_word.append(char7)
-        elif i == 8:
-            char8 = ord(crypt_word[7])
-            char8 -= 8
-            char8 = chr(char8)
-            new_word.append(char8)
-        elif i == 9:
-            char9 = ord(crypt_word[8])
-            char9 -= 9
-            char9 = chr(char9)
-            new_word.append(char9)
-        elif i == 10:
-            char10 = ord(crypt_word[9])
-            char10 -= 10
-            char10 = chr(char10)
-            new_word.append(char10)
+    for x in range(len(crypt_word)):
+        char = crypt_word[x]
+        char = ord(char) - (x + 1)
+
+        new_word.append(chr(char))
 
 
     # Print
-    print(''.join(new_word))
+    print(f'Here is the crypted word =>', ''.join(new_word))
 
 def DNC_logic(): # Main DNC logic
-    crypt_word = input()
+    crypt_word = input('Enter the crypted word => ')
     new_word = []
 
     # For loop
-    for i in range(len(crypt_word) + 1):
-        if i == 1:
-            char1 = ord(crypt_word[0])
-            char1 += 1
-            char1 = chr(char1)
-            new_word.append(char1)
-        elif i == 2:
-            char2 = ord(crypt_word[1])
-            char2 += 2
-            char2 = chr(char2)
-            new_word.append(char2)
-        elif i == 3:
-            char3 = ord(crypt_word[2])
-            char3 += 3
-            char3 = chr(char3)
-            new_word.append(char3)
-        elif i == 4:
-            char4 = ord(crypt_word[3])
-            char4 += 4
-            char4 = chr(char4)
-            new_word.append(char4)
-        elif i == 5:
-            char5 = ord(crypt_word[4])
-            char5 += 5
-            char5 = chr(char5)
-            new_word.append(char5)
-        elif i == 6:
-            char6 = ord(crypt_word[5])
-            char6 += 6
-            char6 = chr(char6)
-            new_word.append(char6)
-        elif i == 7:
-            char7 = ord(crypt_word[6])
-            char7 += 7
-            char7 = chr(char7)
-            new_word.append(char7)
-        elif i == 8:
-            char8 = ord(crypt_word[7])
-            char8 += 8
-            char8 = chr(char8)
-            new_word.append(char8)
-        elif i == 9:
-            char9 = ord(crypt_word[8])
-            char9 += 9
-            char9 = chr(char9)
-            new_word.append(char9)
-        elif i == 10:
-            char10 = ord(crypt_word[9])
-            char10 += 10
-            char10 = chr(char10)
-            new_word.append(char10)
+    for x in range(len(crypt_word)):
+        char = crypt_word[x]
+        char = ord(char) + (x + 1)
+        new_word.append(chr(char))
 
     # Print
     print(''.join(new_word))
@@ -127,7 +34,7 @@ print('When you are done just type Exit!')
 # While loop
 while True:
     # Asking for the function
-    function = input()
+    function = input('Enter the function or type Exit => ')
 
     # Checking for end
     if function == 'Exit':
@@ -145,4 +52,3 @@ while True:
     # Checking if it's not ENC or DNC
     else:
         print('Error')
-
